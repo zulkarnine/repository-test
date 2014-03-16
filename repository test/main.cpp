88348319c5c4c5e0edd9d84abce3dee94f04accb
zulkarnine <zulkarnine2076@gmail.com>
2014-03-17 03:26:41 +0900

    Initial Commit

---
+++ repository test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  repository test
+//
+//  Created by Zulkarnine on 3/17/14.
+//  Copyright (c) 2014 Zulkarnine Mahmud. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
