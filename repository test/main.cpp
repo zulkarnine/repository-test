3787188ae6c9ab0f005a5926330fd9290d7c93a1
zulkarnine <zulkarnine2076@gmail.com>
2014-03-17 03:30:55 +0900

    Update main.cpp

--- repository test/main.cpp
+++ repository test/main.cpp
@@ -8,11 +8,14 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
     std::cout << "Hello, World!\n";
+    cout<<"Just a test";
     return 0;
 }
 
